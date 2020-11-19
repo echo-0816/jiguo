256a4db2c1c94cdf0282743fd445254e1abbf401
goujizhi <912475705@qq.com>
2020-11-19 14:32:10 +0800

    '19'

--- js/login.js
+++ js/login.js
@@ -3,21 +3,21 @@ $('#btn').on('click',function(){
     $.ajax({
 
         url:'http://192.168.1.64:3000/users/login',
-        type:post,
+        type:'post',
         data:{
             username:$('#tel').val(),
             password:$('#password').val()
         },
 
         success:function(res){
-            console(res);
+            console.log(res);
             if(res.msg == '登陆成功！'){
                 window.open('../index.html');
             }
           
         },
 
-        dataType:'json'
+        dataType:'json',
     })
 
 
