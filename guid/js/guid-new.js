d49708096384ad7df43a7466ce35db3f1767d059
hanbo <840718239@qq.com>
2020-11-19 08:42:57 +0800

    'hello'

---
+++ guid/js/guid-new.js
@@ -0,0 +1,22 @@
+$('section>div>p').on('click', function () {
+    // 隐藏p
+    $(this).hide();
+
+    $(function () {
+        $.ajax({
+            url: 'http://192.168.1.64:3000/play/new',
+            success: function (res) {
+                inHtml(res[0]);
+            }
+        });
+    })
+
+    function inHtml(date) {
+        var dot_tem = doT.template($('#list_template').text());
+        $('section>div>ul').html(dot_tem(date));
+        console.log(date);
+    }
+})
+
+
+// 计时器
--- play/js/play-new.js
+++ play/js/play-new.js
@@ -1,16 +1,27 @@
-$(function () {
+$('section>div>p').on('click', function () {
 
-    $.ajax({
-        url: 'http://192.168.1.64:3000/play/new',
-        success: function (res) {
-            inHtml(res[0]);
-        }
-    });
+    // 隐藏p
+    $(this).hide();
+
+
+
+
+    $(function () {
+
+        $.ajax({
+            url: 'http://192.168.1.64:3000/play/new',
+            success: function (res) {
+                inHtml(res[0]);
+            }
+        });
+
+    })
+
+    function inHtml(date) {
+        var dot_tem = doT.template($('#list_template').text());
+        $('section>div>ul').html(dot_tem(date));
+        console.log(date);
+    }
+
 
 })
-
-function inHtml(date) {
-    var dot_tem = doT.template($('#list_template').text());
-    $('section ul').html(dot_tem(date));
-    console.log(date);
-}
